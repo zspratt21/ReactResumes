de1e450e1fa754bb20c0d3c63fe6d5b9043fb383
Zak <zspratt21@gmail.com>
2024-04-12 17:13:44 +0800

    Overhaul skill crud and management forms to follow the same implementation as experience and milestones.

--- resources/js/Pages/Resume/Partials/Skill/CrudSkill.tsx
+++ resources/js/Pages/Resume/Partials/Skill/CrudSkill.tsx
@@ -1,15 +1,15 @@
-import {useForm} from "@inertiajs/react";
 import {Skill} from "@/types/resume";
-import {FormEventHandler, useRef, useState} from "react";
+import {useForm} from "@inertiajs/react";
+import React, {FormEventHandler, useEffect, useRef, useState} from "react";
 import InputLabel from "@/Components/InputLabel";
 import TextInput from "@/Components/TextInput";
 import InputError from "@/Components/InputError";
-import PrimaryButton from "@/Components/PrimaryButton";
 import SaveButton from "@/Components/SaveButton";
-import CompactImageInput from "@/Components/CompactImageInput";
+import PrimaryButton from "@/Components/PrimaryButton";
+import ImageInput from "@/Components/ImageInput";
 
-export default function CrudSkill({className = '', skill, onDelete}: {className: string, skill: Skill|null, onDelete: () => void}) {
-    const { data, setData, post, delete: deleteRequest, errors, processing, recentlySuccessful} = useForm({
+export default function CrudSkill({className = '', skill, resetEditingSkill}: {className?: string, skill: Skill | null, resetEditingSkill: () => void}) {
+    const generateData = (skill: Skill | null) => ({
         id: skill?.id || 0,
         name: skill?.name || '',
         url: skill?.url || '',
@@ -17,8 +17,15 @@ export default function CrudSkill({className = '', skill, onDelete}: {className:
         remove_icon: 0 as 1 | 0,
         _method: 'patch',
     });
+    const { data, setData, post, delete: deleteRequest, errors, processing, recentlySuccessful} = useForm(generateData(skill));
+
+    useEffect(() => {
+        setData(generateData(skill));
+    }, [skill]);
+
     const [refreshKey, setRefreshKey] = useState(0);
     const imageInputRef = useRef(null);
+
     const submit: FormEventHandler = (e) => {
         e.preventDefault();
         post(
@@ -26,88 +33,81 @@ export default function CrudSkill({className = '', skill, onDelete}: {className:
             {
                 preserveScroll: true,
                 onSuccess: (Page) => {
-                    if (data.id === 0) {
-                        onDelete();
+                    if (!skill) {
+                        setData(generateData(null));
                     }
-                    setRefreshKey(refreshKey + 1);
                 },
             }
         );
     };
-    const handleDelete = () => {
-        if(data.id == 0) {
-            onDelete();
-        }
-        else {
-            deleteRequest(
-                route('skill.delete', {data: {id: data.id}}),
-                {
-                    preserveScroll: true,
-                }
-            );
-        }
-    };
-    return(
-        <section className={className}>
-            <form onSubmit={submit} key={refreshKey}>
-                <div className="flex flex-col sm:flex-row sm:space-x-2">
-                    <div className="flex-1">
-                        <InputLabel htmlFor="name" value="Name"/>
 
-                        <TextInput
-                            className="mt-1 block w-full"
-                            value={data.name}
-                            onChange={(e) => setData('name', e.target.value)}
-                            placeholder="&#xf0ad;"
-                            required
-                            isFocused
-                        />
+    const addNew = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        resetEditingSkill();
+    }
 
-                        <InputError className="mt-2" message={errors.name}/>
-                    </div>
+    return (
+        <div className={className}>
+            <form onSubmit={submit}>
+                <div>
+                    <InputLabel htmlFor="name" value="Name"/>
 
-                    <div className="flex-1">
-                        <InputLabel htmlFor="url" value="Website/Link"/>
+                    <TextInput
+                        className="mt-1 block w-full"
+                        value={data.name}
+                        onChange={(e) => setData('name', e.target.value)}
+                        required
+                    />
 
-                        <TextInput
-                            className="mt-1 block w-full"
-                            value={data.url}
-                            onChange={(e) => setData('url', e.target.value)}
-                            placeholder="&#xf0c1;"
-                            isFocused
-                        />
+                    <InputError message={errors.name} className="mt-1"/>
+                </div>
 
-                        <InputError className="mt-2" message={errors.url}/>
-                    </div>
+                <div>
+                    <InputLabel htmlFor="url" value="Url/Website"/>
 
-                    <div className="flex-1">
-                        <InputLabel htmlFor="file_icon" value="Icon"/>
+                    <TextInput
+                        className="mt-1 block w-full"
+                        value={data.url}
+                        onChange={(e) => setData('url', e.target.value)}
+                        required
+                    />
 
-                        <CompactImageInput
-                            ref={imageInputRef}
-                            id="file_icon"
-                            className="mt-1 block w-full min-h-10"
-                            onChange={(e) => setData('file_icon', e.target.files?.[0] || data.file_icon)}
-                            initialPhoto={skill?.icon || null}
-                            setPhotoData={(photo: File | null) => setData('file_icon', photo)}
-                            setRemoveData={(value: 0 | 1) => {
-                                if (value == 1) {
-                                    setData({...data, remove_icon: value, file_icon: null});
-                                } else {
-                                    setData({...data, remove_icon: value});
-                                }
-                            }}
-                        />
+                    <InputError message={errors.url} className="mt-1"/>
+                </div>
 
-                        <InputError className="mt-2" message={errors.file_icon}/>
-                    </div>
+                <div>
+                    <InputLabel htmlFor="file_icon" value="Icon"/>
 
-                    <div className="flex items-end space-x-2">
-                        <SaveButton className="min-h-10" disabled={processing}/>
-                        <PrimaryButton className="min-h-10" onClick={(e) => {e.preventDefault(); handleDelete()}}>&#xf1f8; Delete</PrimaryButton>
-                    </div>
+                    <ImageInput
+                        ref={imageInputRef}
+                        className="mt-1 block w-full"
+                        onChange={(e) => setData('file_icon', e.target.files?.[0] || data.file_icon)}
+                        initialPhoto={skill?.icon || null}
+                        setPhotoData={(photo: File | null) => setData('file_icon', photo)}
+                        setRemoveData={(value: 0 | 1) => {
+                            if (value == 1) {
+                                setData({...data, remove_icon: value, file_icon: null});
+                            } else {
+                                setData({...data, remove_icon: value});
+                            }
+                        }}
+                        previewClassName="max-h-96 w-auto"
+                    />
+
+                    <InputError message={errors.file_icon} className="mt-1"/>
+                </div>
+
+                <div className="flex justify-end mt-4 space-x-2">
+                    {skill && (
+                        <>
+                            <PrimaryButton onClick={addNew}>Add New Skill</PrimaryButton>
+                            <SaveButton disabled={processing}>Save</SaveButton>
+                        </>
+                    ) || (
+                        <PrimaryButton disabled={processing}>Add New Skill</PrimaryButton>
+                    )}
                 </div>
             </form>
-        </section>
+        </div>
     );
 }
--- resources/js/Pages/Resume/Partials/Skill/ManageSkills.tsx
+++ resources/js/Pages/Resume/Partials/Skill/ManageSkills.tsx
@@ -1,56 +1,57 @@
-import PrimaryButton from "@/Components/PrimaryButton";
-import {useForm, usePage} from "@inertiajs/react";
-import {PageProps} from "@/types";
-import {ReactElement, useEffect, useRef, useState} from "react";
+import {Skill} from "@/types/resume";
 import CrudSkill from "@/Pages/Resume/Partials/Skill/CrudSkill";
-import {Skill, SkillPriority} from "@/types/resume";
+import {useEffect, useState} from "react";
+import {usePage, router} from "@inertiajs/react";
+import {PageProps} from "@/types";
+import CrudView from "@/Pages/Resume/Partials/Crud/CrudView";
 import {DragDropContext, Draggable, Droppable, DropResult} from "@hello-pangea/dnd";
+import CrudHeading from "@/Pages/Resume/Partials/Crud/CrudHeading";
+import CrudContainer from "@/Pages/Resume/Partials/Crud/CrudContainer";
+import CrudWrapper from "@/Pages/Resume/Partials/Crud/CrudWrapper";
 
-export default function ManageSkills({className = '', onCompletionChange}: {className?: string, onCompletionChange: (isComplete: boolean) => void}) {
+
+export default function ManageSkills({className = ''}: {className?: string}) {
+    const [editingSkill, setEditingSkill] = useState<Skill | null>(null);
     const user = usePage<PageProps>().props.auth.user;
-    const newSkillsContainerRef = useRef(null);
-    const [newSkillFormComponents, setNewSkillFormComponents] = useState<ReactElement[]>([]);
-    const [skills, setSkills] = useState<Skill[]>(() => user.skills);
-    const { data, setData, post, errors, processing, recentlySuccessful } = useForm({
-        priorities: [] as SkillPriority[],
-        _method: 'patch',
-    });
+    const [skills, setSkills] = useState<Skill[]>(user.skills);
 
-    const addSkill = () => {
-        const newKey = Date.now();
-        setNewSkillFormComponents(prevComponents => [
-            ...prevComponents,
-            <CrudSkill
-                skill={null}
-                key={newKey}
-                onDelete={() => remove(newKey)}
-                className="w-full"
-            />
-        ]);
-    };
-
-    const remove = (keyToDelete: number) => {
-        setNewSkillFormComponents(prevComponents =>
-            prevComponents.filter((component) => component.key !== keyToDelete.toString())
-        );
-    };
-
-    const checkValidSkills = () => {
-        onCompletionChange(skills.length > 0);
-    }
-    useEffect(checkValidSkills, [skills]);
+    // useEffect(() => {
+    //     setEditingSkill(null);
+    //     setskills(user.skills);
+    // }, [experience.id]);
 
     useEffect(() => {
-        setSkills(user.skills);
+        setSkills(user.skills || []);
     }, [user.skills]);
+
+    const resetEditingSkill = () => {
+        setEditingSkill(null);
+    }
+
+    const deleteSkill = (skill: Skill) => {
+        if (window.confirm('Are you sure you want to delete this skill?')) {
+            if (skill.id === editingSkill?.id) {
+                resetEditingSkill();
+            }
+            router.delete(
+                route('skill.delete',
+                    {
+                        id: skill.id,
+                    }),
+                {
+                    preserveScroll: true
+                }
+            );
+        }
+    }
 
     const onDragEnd = (result: DropResult) => {
         if (!result.destination) {
             return;
         }
         const items = Array.from(skills);
-        const [reorderedItem] = items.splice(result.source.index, 1);
-        items.splice(result.destination.index, 0, reorderedItem);
+        const reorderedItem = items.splice(result.source.index, 1);
+        items.splice(result.destination.index, 0, reorderedItem[0]);
         setSkills(items);
         const priorities = items.map((skill, index) => {
             return {
@@ -58,82 +59,70 @@ export default function ManageSkills({className = '', onCompletionChange}: {clas
                 priority: index,
             };
         });
-        setData('priorities', priorities);
+        router.post(
+            route('skills.prioritize'),
+            {
+                priorities: priorities,
+                _method: 'patch',
+            },
+            {
+                preserveScroll: true
+            }
+        );
     }
-    const firstUpdate = useRef(true);
-
-    useEffect(() => {
-       if (firstUpdate.current) {
-           firstUpdate.current = false;
-           return;
-       }
-
-       else {
-           post(route('skills.prioritize'), {preserveScroll: true});
-       }
-
-
-    }, [data.priorities]);
-
 
     return (
         <section className={className}>
-            <header>
-                <h2 className="text-lg font-medium text-gray-900 dark:text-gray-100">Skills</h2>
-
-                <p className="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                    Add, modify, or remove skills from your profile.
-                </p>
-            </header>
-
+            <CrudHeading text="Skills" empty={skills.length < 1}/>
             {skills.length > 0 && (
-                <>
-                    <h3 className="mt-6 text-md font-medium text-gray-900 dark:text-gray-100">Current Skills</h3>
-                    <DragDropContext onDragEnd={onDragEnd}>
-                        <Droppable droppableId="droppable" direction="vertical">
-                            {(provided, snapshot) => (
-                                <div
-                                    {...provided.droppableProps}
-                                    ref={provided.innerRef}
-                                >
-                                    {skills.map((skill, index) => (
-                                        <Draggable key={skill.id} draggableId={skill.id.toString()} index={index}>
-                                            {(provided, snapshot) => {
-                                                return (
-                                                    <div
-                                                        ref={provided.innerRef}
-                                                        className="flex space-x-2 mt-4"
-                                                        {...provided.draggableProps}
-                                                        {...provided.dragHandleProps}
-                                                    >
-                                                        <span className="text-5xl my-auto text-gray-700 dark:text-gray-300">&#xf07d;</span>
-                                                        <CrudSkill skill={skill} onDelete={() => {}} className="w-full"/>
-                                                    </div>
-                                                );
-                                            }}
-                                        </Draggable>
-                                    ))}
-                                    {provided.placeholder}
-                                </div>
-                            )}
-                        </Droppable>
-                    </DragDropContext>
-                </>
+                <DragDropContext onDragEnd={onDragEnd}>
+                    <Droppable droppableId="droppable" direction="vertical">
+                        {(provided, snapshot) => (
+                            <CrudContainer
+                                isDraggingOver={snapshot.isDraggingOver}
+                                ref={provided.innerRef}
+                                {...provided.droppableProps}
+                            >
+                                {skills.map((skill, index) => (
+                                    <Draggable key={skill.id} draggableId={skill.id.toString()} index={index}>
+                                        {(provided, snapshot) => {
+                                            return (
+                                                <CrudWrapper
+                                                    key={index}
+                                                    ref={provided.innerRef}
+                                                    {...provided.draggableProps}
+                                                    {...provided.dragHandleProps}
+                                                >
+                                                    <CrudView
+                                                        className="flex-1"
+                                                        text={skill.name}
+                                                        setEdit={() => {
+                                                            setEditingSkill(skill)
+                                                        }}
+                                                        onDelete={() => {
+                                                            deleteSkill(skill)
+                                                        }}
+                                                    />
+                                                </CrudWrapper>
+                                            );
+                                        }}
+                                    </Draggable>
+                                ))}
+                                {provided.placeholder}
+                            </CrudContainer>
+                        )}
+                    </Droppable>
+                </DragDropContext>
+            ) || (
+                <p className="text-gray-600 dark:text-gray-400">
+                    You haven't added any skills yet. Add one with the form below.
+                </p>
             )}
-
-            {newSkillFormComponents.length > 0 && (
-                <div ref={newSkillsContainerRef}>
-                    <h3 className="mt-6 text-lg font-medium text-gray-900 dark:text-gray-100">New Skills</h3>
-                    {newSkillFormComponents}
-                </div>
-            )}
-
-            <div className="space-x-2 mt-4">
-                <PrimaryButton onClick={addSkill}>&#xf055; Add Skill</PrimaryButton>
-                {skills.length < 1 && (
-                    <PrimaryButton onClick={() => {onCompletionChange(true)}}>&#xf04e; Skip</PrimaryButton>
-                )}
-            </div>
+            <CrudSkill
+                skill={editingSkill}
+                resetEditingSkill={resetEditingSkill}
+                className="mt-4"
+            />
         </section>
     );
 }
